2f743b34af169591b9736bb288942353d77140ed
Thangaram Senthamaraikannan <thangaram611@gmail.com>
2024-09-20 18:13:39 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ function App() {
       <header className="flex flex-col items-center justify-between mb-4 md:flex-row">
        <a href="https://muthaiyan.me">
        <h1 className="text-xl font-bold text-transparent lg:text-2xl bg-clip-text bg-gradient-to-r from-blue-500 to-pink-500 decoration-8">
-          &#123; JSON Beautifier &#125;
+          &#123; My JSON Beautifier &#125;
         </h1>
        </a>
         <p className="text-xs md:text-sm">Pro Tip 💡 : You can directly paste your code here.</p>
